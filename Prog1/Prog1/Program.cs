532f2fdbf5fa28123f74c4ba3fa5ce8ec6febd77
Udaya Bhanu <143161328+bhanu907@users.noreply.github.com>
2024-10-17 14:40:54 +0530

    Add files via upload

---
+++ Prog1/Prog1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 4;
+            double result;
+            Console.WriteLine("\t Welcome to .Net Programming");
+            Console.WriteLine("Addition = {0}", a + b); ;
+            Console.WriteLine("Subtraction = {0}", a - b);
+            Console.WriteLine("Multiplication ={0}", a * b);
+            result = (double)a / b;
+            Console.WriteLine("Division = {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
