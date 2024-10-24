b04475a2bf22fc276aac895185d030cb0a8c4d23
Udaya Bhanu <143161328+bhanu907@users.noreply.github.com>
2024-10-24 14:02:24 +0530

    Add files via upload

---
+++ Expt13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expt13
+{
+    internal class A
+    {
+        public static void x()
+        {
+            Console.WriteLine("This is method x in class A");
+        }
+    }
+    internal class B
+    {
+        public static void y() 
+        {
+            Console.WriteLine("This is method y in class B");
+        }
+    }
+    internal class C
+    {
+        public static void z()
+        {
+            Console.WriteLine("This is method z in class C");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A.x();
+            B.y();
+            C.z();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exp-12.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp6;
+namespace ConsoleApp6
+{
+    internal class Program1
+    {
+        public static void display()
+        {
+            Console.WriteLine("Hello Bhanu");
+            Console.ReadLine();
+        }
+        internal class program2
+        {
+            public static void Main(string[] args)
+            {
+                Program1.display();
+            }
+        }
+    }
+}
